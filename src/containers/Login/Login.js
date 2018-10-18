de900d2b95032efde202202ecf7edf467be4e5dc
inamune <inagakimunenori0612@gmail.com>
2018-10-18 10:37:40 +0900

    ログイン製作完了

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -2,11 +2,61 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 class Login extends Component {
+  state = {
+    data: {
+      mail: '',
+      pass: ''
+    }
+  };
+
+  handleChange = event => {
+    const data = {...this.state.data};
+    data[event.target.name] = event.target.value;
+
+    this.setState({
+      data: data
+    });
+
+    console.log(data);
+  };
+
   render() {
-    return(
-      <div>Hello React</div>
+    return (
+      <Loginwrap>
+        <Logintitle>ログインフォーム</Logintitle>
+        <Inputwrap>
+          <Id>
+            <Input type="input" id="mail" name="mail" placeholder="メールアドレス" value={this.state.data.mail} onChange={this.handleChange}></Input>
+          </Id>
+          <Pass>
+            <Input type="password" id="pass" name="pass" placeholder="パスワード" value={this.state.data.pass} onChange={this.handleChange}></Input>
+          </Pass>
+          <Log>
+            <Logbutton type="submit" value="ログイン"></Logbutton>
+          </Log>
+        </Inputwrap>
+      </Loginwrap>
     );
   }
 }
 
 export default Login;
+
+const Loginwrap = styled.div`
+`
+const Logintitle = styled.h1`
+  text-align: center;
+`
+const Inputwrap = styled.div`
+  text-align: center;
+`
+const Id = styled.div`
+`
+const Pass = styled.div`
+`
+const Log = styled.div`
+`
+const Input = styled.input`
+`
+const Logbutton = styled.input`
+`
